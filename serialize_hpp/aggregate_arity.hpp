bd6682032adbd66160a82872118057a9397bc028
uchenily <uchenily@qq.com>
2024-06-21 13:01:11 +0800

    Use std::decay

--- serialize_hpp/aggregate_arity.hpp
+++ serialize_hpp/aggregate_arity.hpp
@@ -73,8 +73,9 @@ constexpr auto get(type &value) noexcept -> decltype(auto) {
 
 template <typename T>
 constexpr auto num_fields() noexcept -> std::size_t {
-    return aggregate_arity<
-        std::remove_reference_t<std::remove_cv_t<T>>>::size();
+    // return aggregate_arity<
+    //     std::remove_reference_t<std::remove_cv_t<T>>>::size();
+    return aggregate_arity<std::decay<T>>::size();
 }
 
 } // namespace fzto
