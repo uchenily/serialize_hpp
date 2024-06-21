9b243825144a1534f9f353a13d46890588272cef
uchenily <uchenily@qq.com>
2024-06-21 12:16:12 +0800

    Update aggregate_arity

--- serialize_hpp/aggregate_arity.hpp
+++ serialize_hpp/aggregate_arity.hpp
@@ -1,3 +1,4 @@
+#include <tuple>
 #include <type_traits>
 #include <utility>
 
@@ -28,35 +29,45 @@ template <std::size_t index, typename type>
 constexpr auto get(type &value) noexcept -> decltype(auto) {
     constexpr std::size_t arity = aggregate_arity<
         std::remove_reference_t<std::remove_cv_t<type>>>::size();
-    if constexpr (arity == 1) {
-        auto &[p1] = value;
-        if constexpr (index == 0) {
-            return (p1);
+    if constexpr (index < arity) {
+        if constexpr (arity == 1) {
+            auto &[p0] = value;
+            return std::get<index>(std::tie(p0));
+        } else if constexpr (arity == 2) {
+            auto &[p0, p1] = value;
+            return std::get<index>(std::tie(p0, p1));
+        } else if constexpr (arity == 3) {
+            auto &[p0, p1, p2] = value;
+            return std::get<index>(std::tie(p0, p1, p2));
+        } else if constexpr (arity == 4) {
+            auto &[p0, p1, p2, p3] = value;
+            return std::get<index>(std::tie(p0, p1, p2, p3));
+        } else if constexpr (arity == 5) {
+            auto &[p0, p1, p2, p3, p4] = value;
+            return std::get<index>(std::tie(p0, p1, p2, p3, p4));
+        } else if constexpr (arity == 6) {
+            auto &[p0, p1, p2, p3, p4, p5] = value;
+            return std::get<index>(std::tie(p0, p1, p2, p3, p4, p5));
+        } else if constexpr (arity == 7) {
+            auto &[p0, p1, p2, p3, p4, p5, p6] = value;
+            return std::get<index>(std::tie(p0, p1, p2, p3, p4, p5, p6));
+        } else if constexpr (arity == 8) {
+            auto &[p0, p1, p2, p3, p4, p5, p6, p7] = value;
+            return std::get<index>(std::tie(p0, p1, p2, p3, p4, p5, p6, p7));
+        } else if constexpr (arity == 9) {
+            auto &[p0, p1, p2, p3, p4, p5, p6, p7, p8] = value;
+            return std::get<index>(
+                std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8));
+        } else if constexpr (arity == 10) {
+            auto &[p0, p1, p2, p3, p4, p5, p6, p7, p8, p9] = value;
+            return std::get<index>(
+                std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9));
         } else {
-            return;
+            /* extend it by yourself for higher arities */
+            static_assert(false, "Too many arities!");
         }
-    } else if constexpr (arity == 2) {
-        auto &[p1, p2] = value;
-        if constexpr (index == 0) {
-            return (p1);
-        } else if constexpr (index == 1) {
-            return (p2);
-        } else {
-            return;
-        }
-    } else if constexpr (arity == 3) {
-        auto &[p1, p2, p3] = value;
-        if constexpr (index == 0) {
-            return (p1);
-        } else if constexpr (index == 1) {
-            return (p2);
-        } else if constexpr (index == 2) {
-            return (p3);
-        } else {
-            return;
-        }
-    } else /* extend it by yourself for higher arities */ {
-        return;
+    } else {
+        static_assert(index < arity, "Index out of bounds");
     }
 }
 
