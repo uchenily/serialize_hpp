22419d844ca34a1e9d695e93248e5dd2b58efe1b
uchenily <uchenily@qq.com>
2024-06-21 14:20:55 +0800

    Add test: unsigned integer

--- tests/test_fundamental.cpp
+++ tests/test_fundamental.cpp
@@ -1,14 +1,13 @@
 #include "serialize_hpp/debug.hpp"
 #include "serialize_hpp/serialize.hpp"
 
-struct signed_struct {
-    int8_t  i8;
-    int16_t i16;
-    int32_t i32;
-    int64_t i64;
-};
-
-auto main() -> int {
+void test_signed() {
+    struct signed_struct {
+        int8_t  i8;
+        int16_t i16;
+        int32_t i32;
+        int64_t i64;
+    };
     auto ss = signed_struct{
         100,
         200,
@@ -21,4 +20,30 @@ auto main() -> int {
     ASSERT(ss2.i16 == 200);
     ASSERT(ss2.i32 == 300);
     ASSERT(ss2.i64 == 400);
+}
+
+void test_unsigned() {
+    struct unsigned_struct {
+        uint8_t  u8;
+        uint16_t u16;
+        uint32_t u32;
+        uint64_t u64;
+    };
+    auto us = unsigned_struct{
+        100,
+        200,
+        300,
+        400,
+    };
+    auto buf = fzto::serialize<std::string>(us);
+    auto us2 = fzto::deserialize<unsigned_struct>(buf);
+    ASSERT(us2.u8 == 100);
+    ASSERT(us2.u16 == 200);
+    ASSERT(us2.u32 == 300);
+    ASSERT(us2.u64 == 400);
+}
+
+auto main() -> int {
+    test_signed();
+    test_unsigned();
 }
